7b501b9efcdaa30c5807014fa263d29e818acd21
manikagnish <84679762+manik999@users.noreply.github.com>
2021-05-27 00:25:52 +0530

    added some js to check empty inputs

--- script.js
+++ script.js
@@ -7,4 +7,35 @@ const email = document.getElementById("lastname");
 const password = document.getElementById("password");
 
 // error message
-function showError(input, message) {}
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+// show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+function isValid() {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is invalid");
+  }
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (firstname.value === "") {
+    showError(firstname, "Firstname is required");
+  } else {
+    showSuccess(firstname);
+  }
+});

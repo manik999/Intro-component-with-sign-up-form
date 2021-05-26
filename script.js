b5b5f9579d180a6fb0af710e65e9006cbe46ce4d
manikagnish <84679762+manik999@users.noreply.github.com>
2021-05-26 17:36:10 +0530

    made error and success classes

---
+++ script.js
@@ -0,0 +1,10 @@
+"use stict";
+
+const form = document.getElementById("form");
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const email = document.getElementById("lastname");
+const password = document.getElementById("password");
+
+// error message
+function showError(input, message) {}
